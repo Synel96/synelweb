defecbf18c8a009358339bf5c2f988443f4694b7
Szilveszter <nmszilveszter@gmail.com>
2025-08-21 14:20:24 +0200

    connected postgres db

--- backend/synelweb/synelweb/settings.py
+++ backend/synelweb/synelweb/settings.py
@@ -12,6 +12,9 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 
 from pathlib import Path
 import os
+from dotenv import load_dotenv
+
+load_dotenv(os.path.join(os.path.dirname(os.path.dirname(__file__)), ".env"))
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -86,8 +89,12 @@ WSGI_APPLICATION = "synelweb.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": os.environ.get("POSTGRES_DB"),
+        "USER": os.environ.get("POSTGRES_USER"),
+        "PASSWORD": os.environ.get("POSTGRES_PASSWORD"),
+        "HOST": os.environ.get("POSTGRES_HOST"),
+        "PORT": os.environ.get("POSTGRES_PORT", "5432"),
     }
 }
 
