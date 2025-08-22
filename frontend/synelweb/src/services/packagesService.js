27249ae9f59c79da7145db276aa0650fb3eb9360
Szilveszter <nmszilveszter@gmail.com>
2025-08-22 08:51:51 +0200

    bugfix

--- frontend/synelweb/src/services/packagesService.js
+++ frontend/synelweb/src/services/packagesService.js
@@ -3,5 +3,5 @@ import axios from "axios";
 const BASE_URL = import.meta.env.VITE_API_BASE_URL;
 
 export function fetchPackages() {
-  return axios.get(`${BASE_URL}/packages/list/`);
+  return axios.get(`${BASE_URL}packages/list/`);
 }
